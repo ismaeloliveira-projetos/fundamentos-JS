4784d0588c790c08b8c30809de321ed0e7532d72
Ismael Oliveira <ismaoliveira25@gmail.com>
2026-02-11 19:31:20 +0000

    feat: substituição de condicionais por loop para exibir números de 0 a 99

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-const idade = 23 
-if (idade >= 18) {
-    console.log("Você é maior de idade.")
-} else {
-    console.log("Você é menor de idade.")
-}
-
-const banana = "amarela"
-if (banana === "amarela") {
-    console.log("A banana está madura.")
-} else {
-    console.log("A banana não está madura.")
-}
-
-const carro = "vermelho"
-if (carro === "vermelho") {
-    console.log("O carro é vermelho.")
-} else {
-    console.log("O carro não é vermelho.")
+for (let i = 0; i < 100; i++) {
+    console.log(i);
 }
